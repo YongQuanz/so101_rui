9e737de218883f1220e05c438ea69e9b9686fef5
YongQuanz <yongquan6993@gmail.com>
2026-07-03 11:36:55 +0800

    Removed connStatus

--- so101_rui/web/src/main.js
+++ so101_rui/web/src/main.js
@@ -3,12 +3,17 @@ import { CONFIG } from "./config.js";
 import { Logger } from "./components/Logger.js";
 import { ConnectionStatusUI } from "./components/ConnectionStatusUI.js";
 import { RosbridgeClient } from "./connection/RosbridgeClient.js";
+import { MotorStatusUI } from "./components/MotorStatusUI.js";
+import { HardwareController } from "./services/hardwareController.js";
+import { setupUIEventListeners } from "./utils/dom.js";
 
 class Main {
   constructor() {
     this.logger = new Logger("log-box");
     this.connStatus = new ConnectionStatusUI();
     this.ros = new RosbridgeClient(this.logger);
+    this.motoUI = new MotorStatusUI();
+    this.hardware = new HardwareController(this.ros, this.logger, this.motoUI, this.connStatus);
 
     this.connStatus.set("err", "disconnected"); // Set initial state to disconnected
 
@@ -17,12 +22,14 @@ class Main {
       element("btn-connect").textContent = "Disconnect";
       element("btn-connect").className = "btn btn-warning full-width";
       this.logger.log("Topics advertised & subscribed.", "ok");
+      this.hardware.syncState();
     };
 
     this.ros.onClose = () => {
       this.connStatus.set("err", "disconnected");
       element("btn-connect").textContent = "Connect";
       element("btn-connect").className = "btn btn-primary full-width";
+      this.motoUI.set("disconnected");
     };
   }
 
@@ -31,26 +38,9 @@ class Main {
     this.logger.log("SO-101 RUI ready -> connect to rosbridge to start.");
   }
 
-  toggleConnect() {
-    this.ros.connected ? this.disconnect() : this.connect();
-  }
-
-  connect() {
-    const url = element("ws-url").value.trim();
-    this.connStatus.set("", "connecting...");
-    try {
-      this.ros.connect(url);
-    } catch (_) {
-      this.connStatus.set("err", "error");
-    }
-  }
-
-  disconnect() {
-    this.ros.disconnect();
-  }
 }
 
 const app = new Main();
 app.init();
 
-window.toggleConnect = () => app.toggleConnect();
+setupUIEventListeners(app);
