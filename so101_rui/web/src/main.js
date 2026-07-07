3883d8e90ec4dfc58146e01affb09dace7313b45
YongQuanz <yongquan6993@gmail.com>
2026-07-07 21:54:21 +0800

    Added JointTeleopPanel

--- so101_rui/web/src/main.js
+++ so101_rui/web/src/main.js
@@ -5,6 +5,7 @@ import { ConnectionStatusUI } from "./components/ConnectionStatusUI.js";
 import { RosbridgeClient } from "./connection/RosbridgeClient.js";
 import { MotorStatusUI } from "./components/MotorStatusUI.js";
 import { HardwareController } from "./services/hardwareController.js";
+import { JointTeleopPanel } from "./components/JointTeleopPanel.js";
 import { setupUIEventListeners } from "./utils/dom.js";
 
 class Main {
@@ -13,7 +14,18 @@ class Main {
     this.connStatus = new ConnectionStatusUI();
     this.ros = new RosbridgeClient(this.logger);
     this.motoUI = new MotorStatusUI();
-    this.hardware = new HardwareController(this.ros, this.logger, this.motoUI, this.connStatus);
+    this.joints = new JointTeleopPanel(
+      "joints-container",
+      this.ros,
+      this.logger,
+    );
+    this.hardware = new HardwareController(
+      this.ros,
+      this.logger,
+      this.motoUI,
+      this.connStatus,
+      () => this.joints.publish(),
+    );
 
     this.connStatus.set("err", "disconnected"); // Set initial state to disconnected
 
@@ -22,6 +34,8 @@ class Main {
       element("btn-connect").textContent = "Disconnect";
       element("btn-connect").className = "btn btn-warning full-width";
       this.logger.log("Topics advertised & subscribed.", "ok");
+      this.joints.advertiseTrajectoryTopic();
+      this.joints.subscribeToJointStates();
       this.hardware.syncState();
     };
 
@@ -30,14 +44,15 @@ class Main {
       element("btn-connect").textContent = "Connect";
       element("btn-connect").className = "btn btn-primary full-width";
       this.motoUI.set("disconnected");
+      this.joints.resetSyncFlag();
     };
   }
 
   init() {
     element("ws-url").value = CONFIG.rosbridgeUrl;
+    this.joints.build(this);
     this.logger.log("SO-101 RUI ready -> connect to rosbridge to start.");
   }
-
 }
 
 const app = new Main();
